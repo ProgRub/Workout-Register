a16b96604e7d136b9efd9a0f86532db874e74527
Ruben Rodrigues <ruben2000@gmail.com>
2021-07-23 19:51:27 +0100

    Designed the form

---
+++ Workout-Register/Forms/BaseControl.Designer.cs
@@ -0,0 +1,64 @@
+﻿
+namespace Forms
+{
+    partial class BaseControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ButtonBack = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ButtonBack
+            // 
+            this.ButtonBack.BackColor = System.Drawing.Color.DarkTurquoise;
+            this.ButtonBack.Font = new System.Drawing.Font("Showcard Gothic", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.ButtonBack.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ButtonBack.Location = new System.Drawing.Point(0, 9);
+            this.ButtonBack.Name = "ButtonBack";
+            this.ButtonBack.Size = new System.Drawing.Size(42, 41);
+            this.ButtonBack.TabIndex = 5;
+            this.ButtonBack.Text = "<";
+            this.ButtonBack.UseVisualStyleBackColor = false;
+            this.ButtonBack.Click += new System.EventHandler(this.ButtonBack_Click);
+            // 
+            // BaseControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Transparent;
+            this.Controls.Add(this.ButtonBack);
+            this.Name = "BaseControl";
+            this.Size = new System.Drawing.Size(575, 419);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        protected System.Windows.Forms.Button ButtonBack;
+    }
+}
--- Workout-Register/Forms/Window.Designer.cs
+++ Workout-Register/Forms/Window.Designer.cs
@@ -29,13 +29,127 @@ namespace Forms
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Window";
+			this.TextBoxNewWorkout = new System.Windows.Forms.TextBox();
+			this.LabelNewWorkoutName = new System.Windows.Forms.Label();
+			this.LabelWorkouts = new System.Windows.Forms.Label();
+			this.LabelCompletionTime = new System.Windows.Forms.Label();
+			this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+			this.ComboBoxWorkouts = new System.Windows.Forms.ComboBox();
+			this.ButtonAddWorkoutTime = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// TextBoxNewWorkout
+			// 
+			this.TextBoxNewWorkout.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(1)))), ((int)(((byte)(91)))));
+			this.TextBoxNewWorkout.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.TextBoxNewWorkout.Enabled = false;
+			this.TextBoxNewWorkout.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(181)))), ((int)(((byte)(179)))), ((int)(((byte)(174)))));
+			this.TextBoxNewWorkout.Location = new System.Drawing.Point(179, 39);
+			this.TextBoxNewWorkout.Name = "TextBoxNewWorkout";
+			this.TextBoxNewWorkout.Size = new System.Drawing.Size(439, 24);
+			this.TextBoxNewWorkout.TabIndex = 0;
+			// 
+			// LabelNewWorkoutName
+			// 
+			this.LabelNewWorkoutName.AutoSize = true;
+			this.LabelNewWorkoutName.Font = new System.Drawing.Font("Verdana", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+			this.LabelNewWorkoutName.Location = new System.Drawing.Point(10, 41);
+			this.LabelNewWorkoutName.Name = "LabelNewWorkoutName";
+			this.LabelNewWorkoutName.Size = new System.Drawing.Size(163, 17);
+			this.LabelNewWorkoutName.TabIndex = 1;
+			this.LabelNewWorkoutName.Text = "New Workout Name";
+			// 
+			// LabelWorkouts
+			// 
+			this.LabelWorkouts.AutoSize = true;
+			this.LabelWorkouts.Font = new System.Drawing.Font("Verdana", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+			this.LabelWorkouts.Location = new System.Drawing.Point(89, 9);
+			this.LabelWorkouts.Name = "LabelWorkouts";
+			this.LabelWorkouts.Size = new System.Drawing.Size(84, 17);
+			this.LabelWorkouts.TabIndex = 2;
+			this.LabelWorkouts.Text = "Workouts";
+			// 
+			// LabelCompletionTime
+			// 
+			this.LabelCompletionTime.AutoSize = true;
+			this.LabelCompletionTime.Font = new System.Drawing.Font("Verdana", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+			this.LabelCompletionTime.Location = new System.Drawing.Point(35, 75);
+			this.LabelCompletionTime.Name = "LabelCompletionTime";
+			this.LabelCompletionTime.Size = new System.Drawing.Size(138, 17);
+			this.LabelCompletionTime.TabIndex = 3;
+			this.LabelCompletionTime.Text = "Completion Time";
+			// 
+			// dateTimePicker1
+			// 
+			this.dateTimePicker1.CustomFormat = "";
+			this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+			this.dateTimePicker1.Location = new System.Drawing.Point(179, 69);
+			this.dateTimePicker1.Name = "dateTimePicker1";
+			this.dateTimePicker1.ShowUpDown = true;
+			this.dateTimePicker1.Size = new System.Drawing.Size(96, 24);
+			this.dateTimePicker1.TabIndex = 4;
+			this.dateTimePicker1.Value = new System.DateTime(2021, 7, 23, 0, 0, 0, 0);
+			// 
+			// ComboBoxWorkouts
+			// 
+			this.ComboBoxWorkouts.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(1)))), ((int)(((byte)(91)))));
+			this.ComboBoxWorkouts.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.ComboBoxWorkouts.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.ComboBoxWorkouts.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(181)))), ((int)(((byte)(179)))), ((int)(((byte)(174)))));
+			this.ComboBoxWorkouts.FormattingEnabled = true;
+			this.ComboBoxWorkouts.Location = new System.Drawing.Point(179, 6);
+			this.ComboBoxWorkouts.Name = "ComboBoxWorkouts";
+			this.ComboBoxWorkouts.Size = new System.Drawing.Size(439, 24);
+			this.ComboBoxWorkouts.TabIndex = 5;
+			// 
+			// ButtonAddWorkoutTime
+			// 
+			this.ButtonAddWorkoutTime.AutoSize = true;
+			this.ButtonAddWorkoutTime.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+			this.ButtonAddWorkoutTime.BackColor = System.Drawing.Color.Transparent;
+			this.ButtonAddWorkoutTime.FlatAppearance.BorderSize = 0;
+			this.ButtonAddWorkoutTime.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.ButtonAddWorkoutTime.Font = new System.Drawing.Font("Verdana", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+			this.ButtonAddWorkoutTime.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+			this.ButtonAddWorkoutTime.Location = new System.Drawing.Point(342, 69);
+			this.ButtonAddWorkoutTime.Name = "ButtonAddWorkoutTime";
+			this.ButtonAddWorkoutTime.Size = new System.Drawing.Size(122, 27);
+			this.ButtonAddWorkoutTime.TabIndex = 6;
+			this.ButtonAddWorkoutTime.Text = "Add Workout";
+			this.ButtonAddWorkoutTime.UseVisualStyleBackColor = false;
+			// 
+			// Window
+			// 
+			this.AcceptButton = this.ButtonAddWorkoutTime;
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(1)))), ((int)(((byte)(41)))));
+			this.ClientSize = new System.Drawing.Size(639, 101);
+			this.Controls.Add(this.ButtonAddWorkoutTime);
+			this.Controls.Add(this.ComboBoxWorkouts);
+			this.Controls.Add(this.dateTimePicker1);
+			this.Controls.Add(this.LabelCompletionTime);
+			this.Controls.Add(this.LabelWorkouts);
+			this.Controls.Add(this.LabelNewWorkoutName);
+			this.Controls.Add(this.TextBoxNewWorkout);
+			this.Font = new System.Drawing.Font("Verdana", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(181)))), ((int)(((byte)(179)))), ((int)(((byte)(174)))));
+			this.Name = "Window";
+			this.Text = "WorkoutRegister";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
         }
 
-        #endregion
-    }
+		#endregion
+
+		private System.Windows.Forms.TextBox TextBoxNewWorkout;
+		private System.Windows.Forms.Label LabelNewWorkoutName;
+		private System.Windows.Forms.Label LabelWorkouts;
+		private System.Windows.Forms.Label LabelCompletionTime;
+		private System.Windows.Forms.DateTimePicker dateTimePicker1;
+		private System.Windows.Forms.ComboBox ComboBoxWorkouts;
+		private System.Windows.Forms.Button ButtonAddWorkoutTime;
+	}
 }
 
